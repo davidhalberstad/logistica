1b88dcfa1a5ad04e4de46348768dc4742f47fe21
Roberto S. Fernandes D. Guimaraes <fernandesdeguimaraes@gmail.com>
2019-12-30 12:31:40 -0300

    buscador modificado controlador

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,8 +38,8 @@ class HomeController extends Controller
         }
 
 
-        $anios = vehiculo::select('anio_de_produccion')->orderBy('anio_de_produccion','asc')->get();
-        $marca = vehiculo::select('marca')->orderBy('marca','asc')->get();
+        $anios = vehiculo::select('anio_de_produccion')->distinct()->orderBy('anio_de_produccion','asc')->get();
+        $marca = vehiculo::select('marca')->distinct()->orderBy('marca','asc')->get();
         $tipo_vehiculo = tipos_vehiculos::all();
 
         if ($Request->vehiculoBuscado != null && ($Request->marca && $Request->anio && $Request->id_tipo_vehiculo_lista) == null) {
